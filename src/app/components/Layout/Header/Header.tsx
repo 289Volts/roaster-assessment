db5a8e3ee790bdb1c58f5577dbfb3cd706afb7be
Roland Joshua <289volts@gmail.com>
2026-02-05 10:11:43 +0100

    chore: implemented grid layout to separate sections

---
+++ src/app/components/Layout/Header/Header.tsx
@@ -0,0 +1,14 @@
+import { HStack } from '@chakra-ui/react';
+
+const Header = () => {
+	return (
+		<HStack
+			as="header"
+			justify="end"
+		>
+			<div>Header</div>
+		</HStack>
+	);
+};
+
+export default Header;
--- src/app/components/Layout/Layout.tsx
+++ src/app/components/Layout/Layout.tsx
@@ -1,14 +1,25 @@
 import { Provider } from '@/app/components/ui/provider';
+import { Grid, GridItem } from '@chakra-ui/react';
 import { ReactNode } from 'react';
+import Header from './Header/Header';
 import { Sidebar } from './Sidebar';
 
 const Layout = ({ children }: { children: ReactNode }) => {
 	return (
 		<Provider forcedTheme="light">
-			<div>
-				<Sidebar />
-				{children}
-			</div>
+			<Grid
+				templateColumns="16.31rem 1fr"
+				templateRows="repeat(2, 1fr)"
+				height="100vh"
+			>
+				<GridItem rowSpan={2}>
+					<Sidebar />
+				</GridItem>
+				<GridItem>
+					<Header />
+				</GridItem>
+				<GridItem>{children}</GridItem>
+			</Grid>
 		</Provider>
 	);
 };
